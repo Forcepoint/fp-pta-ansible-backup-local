840be582e329d650d1fc300c81b9b7c0f2c7e3c1
Jeremy Cornett <jeremy.cornett@forcepoint.com>
2022-01-12 11:28:13 -0700

    Add log rotation. Add logging to a separate file. Deprecate the 27 python script. Introduce a what-if switch to the process. Don't run everything under become. Assume the user ansible connects with is the user to run under.

--- README.md
+++ README.md
@@ -5,11 +5,23 @@ frequency via a cron job. This role doesn't transfer the tars off the host. This
 by another process. The simplest transfer would be to utilize a mounted drive as the target backup folder so the files 
 are moved automatically when the tar is created.
 
+The user ansible connects with is the user context this process is run under.
+If you want this to run under another user, you'll need to either connect with that user, or use
+these flags when calling this role, assuming you can do password-less sudo to the user in question
+from the ansible user. DISCLAIMER: I have used the below code when running tasks as another user,
+but I have not used it with a role. Hypothetically, it should work.
+
+    become: yes
+    become_user: "{{ target_user_name }}"
+    become_method: sudo
+    become_flags: '-i'
+
 For information about PTA and how to use it with this Ansible role please visit https://github.com/Forcepoint/fp-pta-overview/blob/master/README.md
 
 ## Requirements
 
-Cron, Tar, and Python27 must be installed.
+Cron, Tar, and Python3 must be installed. The user context this role runs under is assumed to already
+have permissions to read from target folder and to create the destination folder.
 
 ## Role Variables
 
@@ -25,6 +37,8 @@ Cron, Tar, and Python27 must be installed.
 * backup_local_month: The month of the year to run the job. Defaults to "*", or every month of the year.
 * backup_local_weekday: The day of the week to run the job. Defaults to "1-5", or Monday through Friday.
 * backup_local_retention_number: The number of backups to retain. 0 means infinite (no cleanup). Defaults to 10.
+* backup_local_what_if: Logs the operations, but does not actually perform the proscribed tasks. 
+  Defaults to 'no'. Useful if you want to double-check what the script will actually do.
 
 ## Dependencies
 
---
+++ files/backup.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+
+"""
+Author: Jeremy Cornett
+Date: 2018-01-02
+Purpose: Create a tar of the specified folder and store it in the specified location. Retain the specified number of
+records.
+"""
+
+import argparse
+import datetime
+import glob
+import os
+import pathlib
+import tarfile
+
+
+def log(path_log, message):
+    """Output the given message to the log file.
+    :param path_log: The path to the log file to append the message to.
+    :type path_log: str
+    :param message: The message to add to the log file.
+    :type message: str
+    :return: None
+    """
+    with open(path_log, 'a') as file_log:
+        message_lines = message.split('\n')
+        if len(message_lines) <= 1:
+            file_log.write("{} {}\n".format(datetime.datetime.now(), message))
+        else:
+            # Make the log easier to read for multiline output.
+            file_log.write("{} {}\n".format(datetime.datetime.now(), message_lines[0]))
+            for line in message_lines[1:]:
+                file_log.write("\t{}\n".format(line))
+
+
+def setup_logging(log_folder, prefix='', retain=10):
+    """Determine the intended log file path and cleanup old logs.
+    :param log_folder: The folder to add the log file to.
+    :type log_folder: str
+    :param prefix: The prefix to give the log files.
+    :type prefix: str
+    :param retain: The number of log files to retain.
+    :type retain: int
+    :return: The path of the log file.
+    :rtype: str
+    """
+    assert os.path.exists(log_folder), "The log folder must already exist."
+
+    # Calculate the intended log path.
+    log_file_path = os.path.join(log_folder, "{}{}.log".format(
+        prefix, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")))
+    assert not os.path.exists(log_file_path), "A log file already exists with that name - {}".format(log_file_path)
+    log(log_file_path, "SCRIPT START")
+
+    # Get all log files in the directory that's not cron.log.
+    list_log_files = glob.glob(os.path.join(log_folder, "{}*.log".format(prefix)))
+    if 'cron.log' in list_log_files:
+        list_log_files.remove('cron.log')
+    # The log files should sort chronologically when sorted lexically.
+    list_log_files.sort()
+
+    # Delete old logs to keep a maximum.
+    if len(list_log_files) > retain:
+        for path_log_file_to_delete in list_log_files[:-1*(retain+1)]:
+            os.remove(path_log_file_to_delete)
+
+    return log_file_path
+
+
+if __name__ == "__main__":
+    # Parse the command line arguments.
+    parser = argparse.ArgumentParser(description="Create a tar of the specified folder and store it in the specified "
+                                                 "location. Retain the specified number of records.")
+    parser.add_argument("name", help="The name to give the tar.")
+    parser.add_argument("target", help="The file or directory to tar up.")
+    parser.add_argument("destination", help="The directory to place the tar.")
+    parser.add_argument("retention", help="The number of tars to retain. Zero means infinite (i.e. don't delete "
+                                          "anything).")
+    parser.add_argument("--what-if", action='store_true', help="Do not perform any deletions, encryptions, "
+                                                               "or transfers.")
+    args = parser.parse_args()
+
+    # Get a log file setup in the same directory as this script.
+    path_log_file = setup_logging(pathlib.Path(__file__).parent.resolve(), prefix=args.name)
+
+    # Initialize
+    path_target = os.path.abspath(os.path.normpath(args.target))
+    path_destination = os.path.abspath(os.path.normpath(args.destination))
+    count_retain = int(float(args.retention))
+    path_tar = os.path.join(path_destination, "{}_{}.tar".format(
+        args.name, datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")))
+
+    # Assumptions
+    assert os.path.exists(path_target), "The target path must exist."
+    assert os.path.exists(path_destination), "The destination path must exist."
+    assert count_retain >= 0, "The retention number ({}) must greater than or equal to zero.".format(args.retention)
+    assert not os.path.exists(path_tar), \
+        "COLLISION - The calculated path for the tar already exists - {}".format(path_tar)
+
+    # Log some basic information.
+    log(path_log_file, "WHAT-IF: {}".format(args.what_if))
+    log(path_log_file, "TARGET: {}".format(path_target))
+    log(path_log_file, "DESTINATION: {}".format(path_destination))
+    log(path_log_file, "TAR: {}".format(path_tar))
+
+    # Tar up the target to the destination folder.
+    if not args.what_if:
+        with tarfile.open(path_tar, "w") as file_tar:
+            file_tar.add(path_target)
+
+    if count_retain != 0:
+        # Check how many tars there are. If there's more than the retention value, delete the older ones. Ensure
+        # that the timestamp for each tar is taken from the filename, not the file attributes.
+        list_tars = glob.glob(os.path.join(path_destination, "{}_*.tar".format(args.name)))
+
+        if len(list_tars) > count_retain:
+            # The files are named in such a way that a lexicographical sort will sort them chronologically as well.
+            list_tars.sort()
+            # The more recent ones we want to retain.
+            for i in range(0, count_retain):
+                list_tars.pop()
+
+            for path_tar_delete in list_tars:
+                log(path_log_file, "DELETE: {}".format(path_tar_delete))
+                if not args.what_if:
+                    os.remove(path_tar_delete)
